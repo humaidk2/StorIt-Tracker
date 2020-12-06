a9e6b49f8f609981088a6a05dcf4f2296d458cb5
Humaid Khan <humaidk2@gmail.com>
2020-12-07 03:11:51 +0400

    Replace deprecated tslint with eslint

--- src/geoIpHelpers.test.ts
+++ src/geoIpHelpers.test.ts
@@ -17,7 +17,7 @@ describe('Geo Ip Helpers', () => {
     describe('Convert country code to Continent', () => {
         // test status codes
         it('it should GET correctly convert continent', () => {
-            convertToContinent('AE').should.not.be.equal('ME')
+            convertToContinent('AE').should.be.equal('ME')
         })
         // should match mock data
         // it('it should match test project', (done) => {
