59ea8eb0e75682f150ce86f99d19a7990cec059f
Humaid Khan <humaidk2@gmail.com>
2020-12-07 02:50:11 +0400

    Create and run the first test

---
+++ src/geoIpHelpers.test.ts
@@ -0,0 +1,32 @@
+export {}
+const chai = require('chai')
+
+// const chaiHttp = require('chai-http')
+import {
+    calculateDistance,
+    distance,
+    getIp,
+    convertToContinent,
+    getRegion,
+} from './geoIpHelpers'
+let expect = chai.should()
+
+// chai.use(chaiHttp)
+
+describe('Geo Ip Helpers', () => {
+    describe('Convert country code to Continent', () => {
+        // test status codes
+        it('it should GET correctly convert continent', () => {
+            convertToContinent('AE').should.be.equal('ME')
+        })
+        // should match mock data
+        // it('it should match test project', (done) => {
+        //     chai.request(server)
+        //         .get('/projects/humaidk2/5')
+        //         .end((err, res) => {
+        //             res.body.should.be.deep.equal(testProject)
+        //             done()
+        //         })
+        // })
+    })
+})
