d06ba3afffcd46eb94eb235f5d8d0edd8d70da86
Humaid Khan <humaidk2@gmail.com>
2020-12-13 21:14:07 +0400

    Change insertion to update if it exists using upsert

--- src/socketRoutes/addServer.ts
+++ src/socketRoutes/addServer.ts
@@ -1,8 +1,8 @@
 export default function (client: any, sequelize: any) {
     client.on('addserver', function (details: any) {
         const post = {
-            serverId: details.deviceId + details.authId,
-            uid: details.authId,
+            Id: details.deviceId + details.authId,
+            uId: details.authId,
             name: details.name,
             storage: details.storage,
             availableStorage: details.storage,
@@ -15,18 +15,10 @@ export default function (client: any, sequelize: any) {
         const socketUpdate = {
             location: client.id,
         }
-        const sql = `INSERT INTO Server SET ? ON DUPLICATE KEY UPDATE ?;`
-        const query = sequelize.query(
-            sql,
-            [post, socketUpdate],
-            function (err: any, result: any) {
-                if (err) {
-                    // socket.emit('Error'+err);
-                    console.log('Error' + err)
-                } else {
-                    console.log('server is Inserted into the database')
-                }
-            }
-        )
+        try {
+            sequelize.models.Server.upsert(post)
+        } catch (error) {
+            console.log(error)
+        }
     })
 }
