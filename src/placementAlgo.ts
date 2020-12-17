2ae4aa5859677b3670901886e77fa18ded9b9959
Humaid Khan <humaidk2@gmail.com>
2020-12-18 03:00:17 +0400

    Reduce placement algo code by 50% by separating code in to modules

--- src/placementAlgo.ts
+++ src/placementAlgo.ts
@@ -9,6 +9,17 @@
 // // rollback
 // // else   23r33
 
+import { getMax, getMin, reRange, reRangeInverse } from './maxMinHelpers'
+import {
+    calculateQuality,
+    checkFileFitPossible,
+    getAvgQuality,
+    getMinRegionLength,
+    initializeNumberOfChunks,
+    resetUsedStorage,
+    sortServers,
+} from './placementHelpers'
+
 // // replace every server id with location in final list
 // // update the server table
 // // commit
@@ -24,166 +35,75 @@
 // console.log(JSON.stringify(serverList, null, 2));
 export default function placementAlgorithm(
     serverList: any,
-    fileSize: any,
-    regions: any
-) {
-    // for every region
-    // sum up the storage
-
-    const sumRegion: any = {}
-    // iterate user defined regions
-    for (let i = 0; i < regions.length; i++) {
-        // this is to ensure if a region appears twice,
-        // then only the first instance will be initialized
-        if (i === regions.indexOf(regions[i])) {
-            sumRegion[regions[i]] = 0
-        }
+    fileSize: number,
+    regions: string[]
+): any {
+    if (!checkFileFitPossible(serverList, regions, fileSize)) {
+        console.log('file failed to fit')
+        console.log('use aws instead')
+        return []
     }
-
-    for (let i = 0; i < serverList.length; i++) {
-        sumRegion[serverList[i].region] += serverList[i].storage
-    }
-    // ensure file can be stored
-    for (let i = 0; i < regions.length; i++) {
-        sumRegion[serverList[i].region] -= fileSize
-        if (sumRegion[serverList[i].region] < 0) {
-            console.log('file failed to fit')
-            console.log('use aws instead')
-            return []
-        }
-    }
-    let initialNumChunks = 0
-    const newServerList: any = []
-    if (fileSize <= 50000000) {
-        initialNumChunks = 1
-    } else if (fileSize <= 200000000) {
-        initialNumChunks = 2
-    } else if (fileSize <= 500000000) {
-        initialNumChunks = 3
-    } else if (fileSize <= 1000000000) {
-        initialNumChunks = 4
-    } else {
-        initialNumChunks = 4
-    }
-    if (initialNumChunks > serverList.length) {
-        initialNumChunks = serverList.length
-    }
-    // let maxStorage = 0,
-    //     maxDistance = 0,
-    //     maxDownTime = 0
-    // let minStorage = serverList[0].storage,
-    //     minDownTime = serverList[0].totalDownTime
-    // let minDistance = 20000
+    const newServerList: any = {}
+    regions.forEach((region: string) => (newServerList[region] = []))
+    let initialNumChunks: number = initializeNumberOfChunks(
+        serverList,
+        fileSize
+    )
     let fileFits = false
     let w1 = 0.2,
         w2 = 0.6,
         w3 = 0.2
-    const maxList = []
-    const minList = []
-    // create a list for each region
-    for (let i = 0; i < regions.length; i++) {
-        if (i == regions.indexOf(regions[i])) {
-            newServerList.push([])
-            maxList.push([0, 0, 0])
-            minList.push([10000000000, 200000, 20000])
-        }
-    }
-    // forget optimization and focus on readability
-    // modularize find max for region in distance, storage, downtime
-    // modularize normalize field name, oldmin, oldmax, new min, new max
-    // pls create separate files for each of these methods
-    // find all maximum and minimums
-    // when we copy to newserverlist, we need to copy only unique regions
-    for (let i = 0; i < serverList.length; i++) {
-        const currIndex = regions.indexOf(serverList[i].region)
-        if (currIndex != -1) {
-            // calculate distance
-            const servDistance = serverList[i].distance //calculateDistance(details.id, serversList[i].location);
-            const storage = serverList[i].storage
-            const downTime = serverList[i].totalDownTime
-            const location = serverList[i].location
-            const serverId = serverList[i].serverId
-            // storage
-            // max server downtime
-            newServerList[currIndex].push({
-                distance: servDistance,
-                storage: storage,
-                currStorage: storage,
-                downTime: downTime,
-                location: location,
-                serverId: serverId,
+    const maxList: any = {}
+    const minList: any = {}
+    newServerList.forEach((region: string) => {
+        maxList[region] = {}
+        minList[region] = {}
+        maxList[region].distance = getMax(serverList, region, 'distance')
+        maxList[region].storage = getMax(serverList, region, 'storage')
+        maxList[region].downTime = getMax(serverList, region, 'totalDownTime')
+        minList[region].maxDistance = getMin(serverList, region, 'distance')
+        minList[region].storage = getMin(serverList, region, 'storage')
+        minList[region].downTime = getMin(serverList, region, 'totalDownTime')
+    })
+    serverList.forEach((server: any) => {
+        if (
+            Object.prototype.hasOwnProperty.call(newServerList, server.region)
+        ) {
+            newServerList[server.region].push({
+                distance: server.distance,
+                storage: server.storage,
+                currStorage: server.storage,
+                downTime: server.totalDownTime,
+                location: server.location,
+                serverId: server.ServerId,
             })
-            if (servDistance > maxList[currIndex][2]) {
-                maxList[currIndex][2] = servDistance
-            }
-            if (servDistance < minList[currIndex][2]) {
-                minList[currIndex][2] = servDistance
-            }
-            if (storage > maxList[currIndex][0]) {
-                maxList[currIndex][0] = storage
-            }
-            if (storage < minList[currIndex][0]) {
-                minList[currIndex][0] = storage
-            }
-            if (downTime > maxList[currIndex][1]) {
-                maxList[currIndex][1] = downTime
-            }
-            if (downTime < minList[currIndex][1]) {
-                minList[currIndex][1] = downTime
-            }
         }
-    }
-    // normalize all the data to between 0 and 100
-    const newMin = 1,
-        newMax = 100
+    })
+    // pass newServerList[region], maxDistance, minDistance
+
     let minRegionLength = newServerList[0].length
-    for (let i = 0; i < newServerList.length; i++) {
-        const maxDistance = maxList[i][2],
-            minDistance = minList[i][2]
-        const maxStorage = maxList[i][0],
-            minStorage: any = minList[i][0]
-        const maxDownTime = maxList[i][1],
-            minDownTime: any = minList[i][1]
-        const oldDistanceRange = maxDistance - minDistance
-        const oldStorageRange = maxStorage - minStorage
-        const oldDownTimeRange = maxDownTime - minDownTime
-        for (let j = 0; j < newServerList[i].length; j++) {
-            if (oldDistanceRange == 0)
-                newServerList[i][j].distance = minDistance
-            else {
-                const newRange = newMax - newMin
-                newServerList[i][j].distance =
-                    newMax -
-                    ((newServerList[i][j].distance - minDistance) * newRange) /
-                        oldDistanceRange +
-                    newMin
-                // newServerList[i][j].distance = newServerList[i][j].distance
-            }
-            if (oldStorageRange == 0)
-                newServerList[i][j].newStorage = minStorage
-            else {
-                const newRange = newMax - newMin
-                newServerList[i][j].newStorage =
-                    ((newServerList[i][j].storage - minStorage) * newRange) /
-                        oldStorageRange +
-                    newMin
-                // newServerList[i][j].newStorage = newServerList[i][j].newStorage
-            }
-            if (oldDownTimeRange == 0)
-                newServerList[i][j].downTime = minDownTime
-            else {
-                const newRange = newMax - newMin
-                newServerList[i][j].downTime =
-                    newMax -
-                    (((newServerList[i][j].downTime - minDownTime) * newRange) /
-                        oldDownTimeRange +
-                        newMin)
-                // newServerList[i][j].downTime = newServerList[i][j].downTime
-            }
-        }
+    for (const region in newServerList) {
+        reRangeInverse(
+            newServerList[region],
+            maxList[region].distance,
+            minList[region].distance,
+            'distance'
+        )
+        reRange(
+            newServerList[region],
+            maxList[region].storage,
+            minList[region].storage,
+            'storage'
+        )
+        reRangeInverse(
+            newServerList[region],
+            maxList[region].downTime,
+            minList[region].downTime,
+            'downTime'
+        )
         // get minimum region server length
-        if (newServerList[i].length < minRegionLength) {
-            minRegionLength = newServerList[i].length
+        if (newServerList[region].length < minRegionLength) {
+            minRegionLength = newServerList[region].length
         }
     }
     // // calculate quality for each region
@@ -196,34 +116,20 @@ export default function placementAlgorithm(
 
     while (!fileFits && initialNumChunks <= minRegionLength && w2 <= 1) {
         // calculate quality for each region
-        for (let i = 0; i < newServerList.length; i++) {
-            for (let j = 0; j < newServerList[i].length; j++) {
-                newServerList[i][j].quality =
-                    newServerList[i][j].distance * w1 +
-                    newServerList[i][j].newStorage * w2 +
-                    newServerList[i][j].downTime * w3
-            }
-            // sort each region descendingly
-            newServerList[i].sort((a: any, b: any) => b.quality - a.quality)
-        }
+        for (const region in newServerList)
+            calculateQuality(newServerList[region], w1, w2, w3)
+        for (const region in newServerList)
+            sortServers(newServerList[region], 'quality')
+
+        // getMinRegionLength(newServerList)
+        const minRegionLength = getMinRegionLength(newServerList)
         // for maybe the smallest region
         // from 0 - 4, add all quality and divide by 4
         // then from 0 - 4, add all quality and divide by 4
-        const avgList = []
         // calculate the avg quality across all the server regions across all regions
-        for (let i = 0; i < minRegionLength; i++) {
-            let avg = 0
-            for (let j = 0; j < newServerList.length; j++) {
-                avg += newServerList[j][i].quality
-            }
-            avg /= newServerList.length
-            for (let j = 0; j < newServerList.length; j++) {
-                newServerList[j][i].averageQuality = avg
-            }
-            avgList.push(avg)
-        }
+        // getAvgQuality
+        const avgList = getAvgQuality(newServerList, minRegionLength)
         fileFits = true
-        // console.log(newServerList)
         finalList = []
         let qSum = 0
         for (let i = 0; i < initialNumChunks; i++) {
@@ -233,27 +139,17 @@ export default function placementAlgorithm(
         // now we just copy the results to the finalList
         //and ensure it fits for all the regions
         for (let i = 0; i < regions.length; i++) {
-            const index = regions.indexOf(regions[i])
-            if (index != i) {
-                // recalculate quality
-                for (let j = 0; j < newServerList[index].length; j++) {
-                    newServerList[index][j].quality =
-                        newServerList[index][j].distance * w1 +
-                        newServerList[index][j].newStorage * w2 +
-                        newServerList[index][j].downTime * w3
-                }
-                // sort each region descendingly by the quality
-                newServerList[index].sort(
-                    (a: any, b: any) => b.quality - a.quality
-                )
-            }
+            // recalculate quality
+            calculateQuality(newServerList[regions[i]], w1, w2, w3)
+            // sort each region descendingly by the quality
+            sortServers(newServerList, 'quality')
             finalList[i] = []
             for (let j = 0; j < initialNumChunks; j++) {
                 finalList[i].push([
-                    newServerList[index][j].serverId,
+                    newServerList[regions[i]][j].serverId,
                     Math.round((avgList[j] * fileSize) / qSum),
                 ])
-                if (finalList[i][j][1] > newServerList[index][j].storage) {
+                if (finalList[i][j][1] > newServerList[regions[i]][j].storage) {
                     if (w2 < 1 && initialNumChunks < minRegionLength) {
                         initialNumChunks++
                         w2 = (w2 * 100 + 0.1 * 100) / 100
@@ -272,7 +168,7 @@ export default function placementAlgorithm(
                     break
                 } else {
                     // reduce the st
-                    newServerList[index][j].storage -= finalList[i][j][1]
+                    newServerList[regions[i]][j].storage -= finalList[i][j][1]
                 }
             }
             if (!fileFits) {
@@ -283,19 +179,8 @@ export default function placementAlgorithm(
         // for every server in the final list
         // add the storage back to the server in newserverlist
         if (!fileFits) {
-            for (let i = 0; i < newServerList.length; i++) {
-                for (let j = 0; j < newServerList[i].length; j++) {
-                    newServerList[i][j].storage =
-                        newServerList[i][j].currStorage
-                }
-            }
+            resetUsedStorage(newServerList)
         }
-        //can u put newserverlist back to it's original stuff
-        //how?
-        // option 1 make a copy and just set it to that
-        // option 2 add the subtracted value from finallist back to newserverlist
-        // option 3 copy it from serverList
-        //option 2 looks the best!!!!!!!
     }
     if (fileFits) {
         // reduce any used memory
@@ -314,6 +199,5 @@ export default function placementAlgorithm(
         console.log('use aws instead')
         return {}
     }
-
     return finalList
 }
